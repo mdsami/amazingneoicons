37e5a4f9aab082ad3c5233512e5f5306f9c54c27
MD SAMI <mdsami.diu@gmail.com>
2020-04-29 21:33:30 +0600

    Package update

--- lib/amazingneoicons.dart
+++ lib/amazingneoicons.dart
@@ -7,4 +7,9 @@ class AmazingNeoIcons {
 
 
   static const IconData abacus = const IconData(0xF002, fontFamily: 'AmazingNeoIconFont', fontPackage: 'amazingneoicons');
+  static const IconData airplane = const IconData(0xF002, fontFamily: 'AmazingNeoIconFont', fontPackage: 'amazingneoicons');
+  static const IconData alarm = const IconData(0xF002, fontFamily: 'AmazingNeoIconFont', fontPackage: 'amazingneoicons');
+
+  static const IconData ambulance = const IconData(0xF002, fontFamily: 'AmazingNeoIconFont', fontPackage: 'amazingneoicons');
+  static const IconData scale = const IconData(0xF002, fontFamily: 'AmazingNeoIconFont', fontPackage: 'amazingneoicons');
 }
